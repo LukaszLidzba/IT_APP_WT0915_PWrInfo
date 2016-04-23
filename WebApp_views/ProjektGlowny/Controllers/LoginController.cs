c7fd16cb6ef419b139fe14972a6de4ecdd739f79
PiotrZalfresso <200819@student.pwr.edu.pl>
2016-04-23 19:13:04 +0200

    dodany controller logowania

---
+++ WebApp_views/ProjektGlowny/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjektGlowny.Models;
+
+namespace ProjektGlowny.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: User
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserModels model, string returnUrl)
+        {
+            //if (ModelState.IsValid && WebSecurity.Login(model.UserName, model.Password))
+            //{
+            //    return RedirectToLocal(returnUrl);
+            //}
+
+            //// If we got this far, something failed, redisplay form
+            //ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+
+        //
+        // POST: /Account/LogOff
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            // HERE CLEAN SESSION - GET RID OFF TAKEN!!!  //PZJ 
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        #region Helpers
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        #endregion
+    }
+}
