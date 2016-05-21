1aecd829135a0168c86b34711da88e379d8489a4
Mariusz Rozanski <200855@student.pwr.edu.pl>
2016-05-21 21:24:07 +0200

    Dodano bibliotekę jQueryUI, datapicker do widoku Wydarzenia.

---
+++ WebApp_views/ProjektGlowny/Scripts/PWrInfo.js
@@ -0,0 +1,10 @@
+﻿function AddDatePicker() {
+    var $datePicker = $(".datepicker");
+
+    if ($datePicker != null)
+        $datePicker.datepicker({
+            inline: true,
+            changeMonth: true,
+            changeYear: true
+        });
+}
