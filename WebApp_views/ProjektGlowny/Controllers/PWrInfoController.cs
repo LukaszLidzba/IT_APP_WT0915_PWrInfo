1d4c775d6338da8c1dc8ffe5f043b023d13d9b33
pansekta <200703@student.pwr.wroc.pl>
2016-05-24 17:51:20 +0200

    jeszcze jeden fix

--- WebApp_views/ProjektGlowny/Controllers/PWrInfoController.cs
+++ WebApp_views/ProjektGlowny/Controllers/PWrInfoController.cs
@@ -1,126 +1,131 @@
-﻿using System;
+﻿using ProjektGlowny.DataQueryService;
+using ProjektGlowny.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ProjektGlowny.Models;
-using ProjektGlowny.DataQueryService;
 
 namespace ProjektGlowny.Controllers
 {
-    public class PWrInfoController : Controller
+  public class PWrInfoController : Controller
+  {
+    // GET: PWrInfo
+
+    public ActionResult DeansOfficesEdit()
     {
-        // GET: PWrInfo
-
-        public ActionResult DeansOfficesEdit()
-        {
-            return View();
-        }
-        public ActionResult DeansOffices()
-        {
-            return View();
-        }
-        public ActionResult UnitsEdit()
-        {
-            return View();
-        }
-        public ActionResult Units()
-        {
-            return View();
-        }
-        public ActionResult LibrariesEdit()
-        {
-            return View();
-        }
-        public ActionResult Libraries()
-        {
-            return View();
-        }
-        public ActionResult Data()
-        {
-            return View();
-        }
-        public ActionResult EventsEdit()
-        {
-            return View();
-        }
-        public ActionResult Events()
-        {
-            return View();
-        }
-        public ActionResult MessagesEdit()
-        {
-            return View();
-        }
-        public ActionResult Messages()
-        {
-            return View();
-        }
-        public ActionResult UsersEdit()
-        {
-            UserModels model = new UserModels();
-            return View(model);
-        }
-
-
-        [HttpPost]
-        public ActionResult UsersEdit(UserModels model)
-        {
-            if (model.Password == model.repeatPassword)
-            {
-                model.addUser(new Guid(Session["UserTicket"].ToString()), model.name, model.surname, model.Password, model.Login, 0, true);
-
-                return Redirect("~/PWrInfo/Users");
-            }
-
-            ModelState.AddModelError("", "The user name or password provided is incorrect.");
-            return View(model);
-        }
-
-        public ActionResult UserProfile()
-        {
-            if (Session["UserTicket"] != null)
-            {
-                UserModels u = new UserModels();
-
-                UserModels user = u.GetUser(new Guid(Session["UserTicket"].ToString()));
-
-                return View(user);
-
-            }
-            return Redirect("~/Login/Login");
-        }
-
-        public ActionResult Users()
-        {
-            if (Session["UserTicket"] != null)
-            {
-                UserModels u = new UserModels();
-             
-                IList<UserModels> users = u.GetUsers(new Guid(Session["UserTicket"].ToString())).ToList();
-
-                return View(users);
-
-            }
-            return Redirect("~/Login/Login");
-        }
-      
-        public ActionResult Index()
-        {
-            if(Session["UserTicket"] != null)
-            {
-                MessagesModel m = new MessagesModel();
-                EventsModel e = new EventsModel();
-  
-                var tuple = new Tuple<List<MessagesModel>, List<EventsModel>>
-                    (m.GetMessages(new Guid(Session["UserTicket"].ToString())).ToList(), e.GetEvents(new Guid(Session["UserTicket"].ToString())).ToList());
-               
-                return View(tuple);
-
-            }
-            return Redirect("~/Login/Login");
-        }
-
-      
+      return View();
     }
+
+    public ActionResult DeansOffices()
+    {
+      return View();
+    }
+
+    public ActionResult UnitsEdit()
+    {
+      return View();
+    }
+
+    public ActionResult Units()
+    {
+      return View();
+    }
+
+    public ActionResult LibrariesEdit()
+    {
+      return View();
+    }
+
+    public ActionResult Libraries()
+    {
+      return View();
+    }
+
+    public ActionResult Data()
+    {
+      return View();
+    }
+
+    public ActionResult EventsEdit()
+    {
+      return View();
+    }
+
+    public ActionResult Events()
+    {
+      return View();
+    }
+
+    public ActionResult MessagesEdit()
+    {
+      return View();
+    }
+
+    public ActionResult Messages()
+    {
+      return View();
+    }
+
+    public ActionResult UsersEdit()
+    {
+      UserModels model = new UserModels();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult UsersEdit(UserModels model)
+    {
+      if (model.Password == model.repeatPassword)
+      {
+        model.addUser(new Guid(Session["UserTicket"].ToString()), model.name, model.surname, model.Password, model.Login, 1, model.isAdmin);
+
+        return Redirect("~/PWrInfo/Users");
+      }
+
+      ModelState.AddModelError("", "The user name or password provided is incorrect.");
+      return View(model);
+    }
+
+    public ActionResult UserProfile()
+    {
+      if (Session["UserTicket"] != null)
+      {
+        UserModels u = new UserModels();
+
+        UserModels user = u.GetUser(new Guid(Session["UserTicket"].ToString()));
+
+        return View(user);
+      }
+      return Redirect("~/Login/Login");
+    }
+
+    public ActionResult Users()
+    {
+      if (Session["UserTicket"] != null)
+      {
+        UserModels u = new UserModels();
+
+        IList<UserModels> users = u.GetUsers(new Guid(Session["UserTicket"].ToString())).ToList();
+
+        return View(users);
+      }
+      return Redirect("~/Login/Login");
+    }
+
+    public ActionResult Index()
+    {
+      if (Session["UserTicket"] != null)
+      {
+        MessagesModel m = new MessagesModel();
+        EventsModel e = new EventsModel();
+
+        var tuple = new Tuple<List<MessagesModel>, List<EventsModel>>
+            (m.GetMessages(new Guid(Session["UserTicket"].ToString())).ToList(), e.GetEvents(new Guid(Session["UserTicket"].ToString())).ToList());
+
+        return View(tuple);
+      }
+      return Redirect("~/Login/Login");
+    }
+  }
 }
