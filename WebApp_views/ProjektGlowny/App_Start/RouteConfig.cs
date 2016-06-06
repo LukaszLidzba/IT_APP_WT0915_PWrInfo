1667f4d5ed7f734dfe1ed3058f2dc99537a63c67
pansekta <200703@student.pwr.wroc.pl>
2016-06-06 17:49:31 +0200

    ulepszona scieżka

--- WebApp_views/ProjektGlowny/App_Start/RouteConfig.cs
+++ WebApp_views/ProjektGlowny/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace ProjektGlowny
                 url: "{controller}/{action}/{id}",
                 defaults: new { action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+    name: "Start",
+    url: "",
+    defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+);
         }
     }
 }
